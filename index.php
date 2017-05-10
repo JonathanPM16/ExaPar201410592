e0e8eba2575f5e5b609039de0b56b1dd69d645a5
JonathanPM16 <jonathanperalesm@gmail.com>
2017-05-10 11:24:34 -0500

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
    <td>
    <table>
     <tr>
-     <td width="100%"><h3>For data scientists</h3></td>
+     <td><h3>For data scientists</h3></td>
     </tr>
    <?php
   $servername = "us-cdbr-iron-east-03.cleardb.net";
@@ -33,8 +33,8 @@
       // output data of each row
       while($row1 = $result1->fetch_assoc()) {?>
       <tr>
-      <td width="30%"><img src="<?php echo $row1['imagen_servicio']?>"></td>
-      <td width="70%"><?php echo $row1['nombre_servicio']?> </br><?php echo $row1['descripcion_servicio']?> </td>
+      <td><img src="<?php echo $row1['imagen_servicio']?>"></td>
+      <td><?php echo $row1['nombre_servicio']?> </br><?php echo $row1['descripcion_servicio']?> </td>
       </tr>
      <?php }
   } else {
@@ -46,7 +46,7 @@
  </td>
     <td>
     <table>
-     <td width="100%"><h3>For IT professionals</h3></td>
+     <td><h3>For IT professionals</h3></td>
    <?php
   $servername = "us-cdbr-iron-east-03.cleardb.net";
   $username = "b74ba3320e82ec";
@@ -64,8 +64,8 @@
       // output data of each row
       while($row2 = $result2->fetch_assoc()) {?>
       <tr>
-      <td width="30%"><img src="<?php echo $row2['imagen_servicio']?>"></td>
-      <td width="70%"><?php echo $row2['nombre_servicio']?> </br><?php echo $row2['descripcion_servicio']?> </td>
+      <td><img src="<?php echo $row2['imagen_servicio']?>"></td>
+      <td><?php echo $row2['nombre_servicio']?> </br><?php echo $row2['descripcion_servicio']?> </td>
       </tr>
      <?php }
   } else {
